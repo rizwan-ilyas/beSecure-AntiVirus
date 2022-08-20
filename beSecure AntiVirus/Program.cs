7ce80e31f1d3fb02fbb1a42e4d8340a8dcf7ef42
rizwan-ilyas <rizwanilyas.me@gmail.com>
2022-08-20 19:34:41 +0500

    test

--- beSecure AntiVirus/Program.cs
+++ beSecure AntiVirus/Program.cs
@@ -17,7 +17,7 @@ namespace beSecure_AntiVirus
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-
+            // testing commit
         }
     }
 }
